ede918e70da4610270f8474edec71b732cdcd839
shimale <shimon_i@yahoo.com>
2013-09-29 21:25:42 +0300

    Free files

---
+++ projectfinish/breadcrumbs.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<span id='headertitle'>". $title . "</span>" ;
+
+	echo '<a  data-theme="e" id="back" data-role="button" href="#" data-rel="back">אחורה</a>';
+
+?>
---
+++ projectfinish/userInterface.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+	if(isset(Yii::app()->session['user'])){
+?>
+
+<ul class="user" data-role="listview" data-inset="true" data-divider-theme="b">
+	   <li data-role="list-divider">ממשק משתמש</li>
+	    <li data-theme="d"> משתמש: <?php 
+	    echo 	 Yii::app()->session['user']->getUsername(); ?></li>
+		 <li data-theme="d"> <a href="<?php echo $this->createUrl("Main/logout") ?>">התנתק </a></li>
+</ul>
+<?php
+ }else{
+
+ 	$url =$this->createUrl("Main/index");
+ 	header('Location: '.$url);
+ }
